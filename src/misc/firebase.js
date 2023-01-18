f83e338f92efe7b09bf3b50cd6f77844851c65fe
selma <selma.om1@gmail.com>
2023-01-18 20:50:27 +0100

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+const config = {
+    apiKey: "AIzaSyCVu8HHcba5YQCYzM2Iw7-BweMzpjf7AbM",
+    authDomain: "chat-app-772e3.firebaseapp.com",
+    databaseURL: "https://chat-app-772e3-default-rtdb.firebaseio.com",
+    projectId: "chat-app-772e3",
+    storageBucket: "chat-app-772e3.appspot.com",
+    messagingSenderId: "224934355798",
+    appId: "1:224934355798:web:840be9ecca0fea8d6b6cf7",
+    measurementId: "G-LVHZDJ85KF"
+  };
+  
+  // Initialize Firebase
+  const app = firebase.initializeApp(config);
