28e3609337df4df87f984832de3809cb6aaa70b2
selmsy <selma.om1@gmail.com>
2023-01-18 18:33:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/main.scss';
+import 'rsuite/dist/rsuite.min.css';
+
+function App() {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default App;
